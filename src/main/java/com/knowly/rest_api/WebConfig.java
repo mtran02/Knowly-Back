3ac520bdc2924812c8b3214719357c63e0b15803
mtran02 <156412904+mtran02@users.noreply.github.com>
2025-01-17 08:05:13 +0100

    Update WebConfig.java

--- src/main/java/com/knowly/rest_api/WebConfig.java
+++ src/main/java/com/knowly/rest_api/WebConfig.java
@@ -9,14 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*") // Applique CORS à tous les endpoints de votre API
-                .allowedOrigins("https://knowly-app-front-next-88aa-4lk4r4fd3-bakops-projects.vercel.app"/) // Remplacez
-                                                                                                           // par l'URL
-                                                                                                           // de votre
-                                                                                                           // app
-                                                                                                           // Next.js
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Autorise ces méthodes HTTP
-                .allowedHeaders("") // Autorise tous les headers
-                .allowCredentials(true); // Permet l'envoi de cookies ou d'informations d'authentification
+        registry.addMapping("/*")
+                .allowedOrigins("https://knowly-app-front-next-88aa-4lk4r4fd3-bakops-projects.vercel.app"/)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("")
+                .allowCredentials(true);
     }
 }
