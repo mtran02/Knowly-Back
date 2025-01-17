a4fb42f4f718979502aeebf819c7095f49b3ad08
mtran02 <156412904+mtran02@users.noreply.github.com>
2025-01-17 10:55:58 +0100

    Update WebConfig.java

--- src/main/java/com/knowly/rest_api/WebConfig.java
+++ src/main/java/com/knowly/rest_api/WebConfig.java
@@ -9,10 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*")
-                .allowedOrigins("https://knowly-app-front-next-88aa-4lk4r4fd3-bakops-projects.vercel.app/")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("")
-                .allowCredentials(true);
+        registry.addMapping("/**")  // Allow all paths
+                .allowedOrigins("https://knowly-app-front-next-88aa-omega.vercel.app")  // Your frontend URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed HTTP methods
+                .allowedHeaders("*")  // Allow all headers (or specify specific headers)
+                .allowCredentials(true);  // Allow credentials (like cookies)
     }
 }
